42484ced3d12561cf2afe7a0c6f15520f3ad477c
boyaneck <jinxx93@naver.com>
2024-08-08 19:14:18 +0900

    part1 강의 듣기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <p>
+      {/* twich clone part 1/2 의 강의 반 정도 들음 */}
       Hello therzze
       <Button size="sm">click</Button>
     </p>
