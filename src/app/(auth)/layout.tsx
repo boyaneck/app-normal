1577f44c9c6ad3dc0da4524cbad09be4164c5bbe
boyaneck <jinxx93@naver.com>
2024-08-11 23:17:32 +0900

    코테문제풀이

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      인증 레이아웃
+      //코테 문제풀이 했음 인증 레이아웃
       {children}
     </div>
   );
