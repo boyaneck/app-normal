68ebe0716f47ca21e6a1e852bd970f50196e63a5
boyaneck <jinxx93@naver.com>
2024-08-20 23:25:12 +0900

    supabase 연동 및 로그인 시작

--- src/app/(browse)/_components/container.tsx
+++ src/app/(browse)/_components/container.tsx
@@ -1,13 +1,19 @@
 "use client";
-import React from "react";
-
+import { cn } from "@/lib/utils";
+import { useSidebarStore } from "@/store/sidebar_store";
+import React, { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 const Container = ({ children }: ContainerProps) => {
+  const { collapsed } = useSidebarStore((state) => state);
+  const matches = useMedia;
   return (
-    <div>
+    <div
+      className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}
+    >
       Container
       {children}
     </div>
---
+++ src/app/(browse)/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      Pageasdsaadsadssd역는 사인 에핑기지 에기ㅓㅈ닷이사인 페이지 입니다
+    </div>
+  );
+};
+
+export default Page;
