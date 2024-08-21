970160f0f6264041255e67503cd5988ce87ff5f3
boyaneck <jinxx93@naver.com>
2024-08-21 23:24:22 +0900

    프로젝터 구조 수정중

---
+++ src/store/user.ts
@@ -0,0 +1,13 @@
+// import { create } from "zustand";
+
+// interface userState {
+//   user: userData | undefined;
+//   setUser: (user: null) => void;
+// }
+
+// const useUserStore = create<userState>((set) => ({
+//   user: undefined,
+//   setUser: (user) => set(user),
+// }));
+
+// export default useUserStore;
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+declare interface userData {
+  id: string;
+  user_nickname: string;
+  avatar_url: string;
+  email: string;
+  created_at: string;
+  isLive: boolean;
+}
