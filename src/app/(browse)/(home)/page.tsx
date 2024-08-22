33ce6a9a536727ccb349734d66580ccade6079c9
boyaneck <jinxx93@naver.com>
2024-08-22 23:20:54 +0900

    오늘은 코테만 하였다, 다음부터는 프로젝트를 위한 커밋을 할것...

--- src/app/(browse)/(home)/page.tsx
+++ src/app/(browse)/(home)/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    //후우 코테부터일단...
     <p>
       {/* twich clone part 1/2 의 강의 반 정도 들음 */}
       <Button size="sm">click</Button>
