fa04c37d25ca73c3e75d4c3fcbc1d5b4212765de
boyaneck <jinxx93@naver.com>
2024-08-26 23:16:16 +0900

    supabase 셋업

--- src/app/(browse)/signin/page.tsx
+++ src/app/(browse)/signin/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Page = () => {
+const Signin = () => {
   return (
     <div>
-      Pageasdsaadsadssd역는 사인 에핑기지 에기ㅓㅈ닷이사인 페이지 입니다
+      Signinasdsaadsadssd역는 사인 에핑기지 에기ㅓㅈ닷이사인 페이지 입니다
     </div>
   );
 };
 
-export default Page;
+export default Signin;
