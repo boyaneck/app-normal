f4604204c1b1e03f6dbdccd6604d6123e8fbdbe7
boyaneck <jinxx93@naver.com>
2024-08-27 23:23:50 +0900

    구글 소셜로그인 기능 완료

--- src/app/(browse)/signin/page.tsx
+++ src/app/(browse)/signin/page.tsx
@@ -1,9 +1,37 @@
+"use client";
+import { supabaseForClient } from "@/supabase/supabase_client";
+import Image from "next/image";
 import React from "react";
 
 const Signin = () => {
+  const getGoogleSocialLogin = async () => {
+    try {
+      await supabaseForClient.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          queryParams: {
+            access_type: "offline",
+            prompt: "consent",
+          },
+          // redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}${url}`,
+        },
+      });
+    } catch (error) {
+      // openCustomModalHandler(`goolgleAuthFailed: ${error}`, "alert")
+    }
+  };
   return (
-    <div>
-      Signinasdsaadsadssd역는 사인 에핑기지 에기ㅓㅈ닷이사인 페이지 입니다
+    <div className="flex flex-col border border-black  items-center">
+      <div onClick={getGoogleSocialLogin}>
+        <Image
+          src={"/images/google_logo.png"}
+          alt="google_logo"
+          width={35}
+          height={35}
+        ></Image>
+      </div>
+      <div>깃허브 로그인</div>
+      <div> 로그인</div>
     </div>
   );
 };
--- src/supabase/supabase_client.ts
+++ src/supabase/supabase_client.ts
@@ -1,5 +1,15 @@
-// import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = 'https://fwobxlwofswxuyttzase.supabase.co'
-// const supabaseKey = process.env.SUPABASE_KEY
-// const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
+
+console.log("꺄르르르르르르르르를", supabaseUrl);
+console.log("슈퍼키", supabaseKey);
+const supabaseForClient = createClient(
+  supabaseUrl as string,
+  supabaseKey as string
+);
+
+console.log(supabaseKey, "키");
+console.log(supabaseUrl, "URL");
+export { supabaseForClient };
