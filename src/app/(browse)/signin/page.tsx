a6c3598350a442cfc92941bf43b01081ee6e64c0
boyaneck <jinxx93@naver.com>
2024-08-29 23:10:19 +0900

    branch 이동해서 작업하기

--- src/app/(browse)/signin/page.tsx
+++ src/app/(browse)/signin/page.tsx
@@ -20,6 +20,19 @@ const Signin = () => {
       // openCustomModalHandler(`goolgleAuthFailed: ${error}`, "alert")
     }
   };
+
+  const getKakaoSocialLogin = async () => {
+    try {
+      await supabaseForClient.auth.signInWithOAuth({
+        provider: "kakao",
+        options: {
+          // redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}${url}`,
+        },
+      });
+    } catch (error) {
+      // openCustomModalHandler(`kakaoAuthFailed: ${error}`, "alert")
+    }
+  };
   return (
     <div className="flex flex-col border border-black  items-center">
       <div onClick={getGoogleSocialLogin}>
@@ -30,8 +43,22 @@ const Signin = () => {
           height={35}
         ></Image>
       </div>
-      <div>깃허브 로그인</div>
-      <div> 로그인</div>
+      <div className="" onClick={getKakaoSocialLogin}>
+        <Image
+          src={"/images/kakao_logo.png"}
+          alt="kakao_logo"
+          width={35}
+          height={35}
+        ></Image>
+      </div>
+      {/* <div className="">
+        <Image
+          src={"/images/github_logo.png"}
+          alt="github_logo"
+          width={35}
+          height={35}
+        ></Image>
+      </div> */}
     </div>
   );
 };
