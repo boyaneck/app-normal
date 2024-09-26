97129893b758eed2214b39ac2a0d3c0ce151c731
boyaneck <jinxx93@naver.com>
2024-09-26 23:26:18 +0900

    수정한게 없고 구조를 어떻게 다시 짜야할지 구성만 했다. 다 시작하자

--- src/hooks/useFollow.ts
+++ src/hooks/useFollow.ts
@@ -1,44 +1,32 @@
-import { addFollow } from "@/api/follow"
-import { QueryCache, QueryClient, useMutation } from "@tanstack/react-query"
-import { useEffect } from "react"
+import { addFollow } from "@/api/follow";
+import { QueryCache, QueryClient, useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-
-interface FollowProps{
-    user_email:string
+interface FollowProps {
+  user_email: string;
 }
 
+const useFollow = (user_email: FollowProps) => {
+  const queryClient = new QueryClient();
 
-
-const useFollow = (string:FollowProps)=>{
-
-const queryClient=new QueryClient();
-
-
-useEffect(()=>{
-  const followMutation=useMutation(,{
-    onMutate:async (user_email) =>{
-    console.log("onMutate 호출")
-    await queryClient.cancelQueries({queryKey:["followUser"]}
-      
-    )
-    const previousFollowUser=queryClient.getQueryData(["followUser"])
-    queryClient.setQueryData(["followUser"],(user:string)=>[...user,user_email])
-    return {previousFollowUser}
-  },
-     onError:(context)=>{
-      console.log("follow mutate 중 에러 발생 :",context)
-      queryClient.setQueryData(["followUser"],context.previousFollowUser)
-     },
-     onSettled:()=>{
-      console.log("onSettled")
-      queryClient.invalidateQueries({queryKey:["followUser"
-      ]})
-     }
-  },)
-
-},[])
-
-
-}
-
-
+  const followMutation = useMutation( {
+    mutationFn:addFollow(user_email),
+    onMutate: async (user_email: FollowProps) => {
+      console.log("onMutate 호출");
+      await queryClient.cancelQueries({ queryKey: ["followUser"] });
+      const previousFollowUser = queryClient.getQueryData<string[]>(["followUser"]);
+      queryClient.setQueryData(["followUser"], (old: string[] | undefined) => {
+        return old? [...old,user_email] :[user_email]
+      });
+      return { previousFollowUser };
+    },
+    onError: (context) => {
+      console.log("follow mutate 중 에러 발생 :", context);
+      queryClient.setQueryData(["followUser"], context?.previousFollowuser);
+    },
+    onSettled: () => {
+      console.log("onSettled");
+      queryClient.invalidateQueries({ queryKey: ["followUser"] });
+    },
+  });
+};
