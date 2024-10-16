fb7ec5146aa53a7c3056237bf5aa80509dd8c118
boyaneck <jinxx93@naver.com>
2024-10-16 23:35:28 +0900

    [개념공부] livekit과 Ingress 공부 및 Ingress와 livekit의 wephook 작업

--- src/api/ingress.ts
+++ src/api/ingress.ts
@@ -6,11 +6,32 @@ import {
   IngressClient,
   IngressVideoEncodingPreset,
   RoomServiceClient,
-  TrackSource,
+  type CreateIngressOptions,
 } from "livekit-server-sdk";
 
+import {} from "livekit-server-sdk/dist/proto/livekit_models";
 const roomService = new RoomServiceClient(
   process.env.LIVEKIT_API_URL!,
   process.env.LIVEKIT_API_KEY!,
   process.env.LIVEKIT_API_SECRET!
 );
+
+export const createIngress = async (ingressType: IngressInput) => {
+  //  const self=await getSelf()
+
+  const options: CreateIngressOptions = {
+    name: "",
+    roomName: "",
+    participantName: "",
+    participantIdentity: "",
+  };
+
+  if (ingressType === IngressInput.WHIP_INPUT) {
+    options.bypassTranscoding = true;
+  } else {
+    options.video = {
+      source: TrackSource.CAMERA,
+      preset: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+    };
+  }
+};
