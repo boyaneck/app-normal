59ace826bae83bd60f5d7283d1ea6e145dab7644
boyaneck <jinxx93@naver.com>
2025-02-10 22:54:03 +0900

    [BUILD] 날짜변환 dayjs 설치

--- src/app/(browse)/chat/chat_room.tsx
+++ src/app/(browse)/chat/chat_room.tsx
@@ -4,19 +4,22 @@ import useUserStore from "@/store/user";
 import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
+import daysjs from "dayjs";
 
 const ChatRoom = () => {
-  // const [message, setmessage] = useState("");
-  // const [receiveMessageInfo, setReceiveMessageInfo] = useState<
-  //   props_message_info[]
-  // >([]);
-  // const [welcomeMessage, setWelcomeMessage] = useState("");
-  // const [messageInfo, setMessageInfo] = useState({});
-  // const { chatRoomInfo } = useChatRoomInfo();
-  // const socket = io("http://localhost:3001/room", {
-  //   transports: ["websocket"],
-  // });
-  // const userInfo = useUserStore((state) => state.user);
+  const [message, set_message] = useState("");
+  const [receive_message_info, set_receive_message_info] = useState<
+    props_message_info[]
+  >([]);
+  const [welcome_message, set_welcome_message] = useState("");
+  const [message_info, set_message_info] = useState({});
+  const socket = io("http://localhost:3001/room", {
+    transports: ["websocket"],
+  });
+  dayjs.locale("ko"); // 로케일 설정
+
+  console.log("쭈르르릅");
+  const user_info = useUserStore((state) => state.user);
   // const { currentChatRoomNumber } = useCurrentChatRoomStore();
   // const chat_room_number = currentChatRoomNumber;
   // const { isChatRoomOpened, setIsChatRoomOpened } = useChatRoomOpenStore(
@@ -24,62 +27,65 @@ const ChatRoom = () => {
   // );
   // const { rooms, setMessages } = useChatStore();
 
-  // const { data: chatInfo } = useQuery(
-  //   queryFn: getChatInfo,
-  // });
-  // console.log("data가 나오나 ?", chatInfo);
+  const { data: chatInfo } = useQuery({
+    queryKey: ["getChatInfo"],
+    queryFn: getChatInfo,
+  });
 
-  // useEffect(() => {
-  //   socket.on("receiveMessage", (messageInfo: props_message_info) => {
-  //     setReceiveMessageInfo((prev) => [...prev, messageInfo]);
-  //   });
+  useEffect(() => {
+    socket.on("connection", () => {
+      alert("연결성공");
+    });
+    socket.on("receiveMessage", (message_info: props_message_info) => {
+      // setReceiveMessageInfo((prev) => [...prev, message_info]);
+    });
 
-  //   return () => {};
-  // }, [socket, rooms]);
+    return () => {};
+  }, [socket]);
 
   // socket.on("welcomeMessage", (welcomeMsg) => {
   //   setWelcomeMessage(welcomeMsg);
   // });
 
-  // let onHandlerSendMessage = () => {
-  //   const user_nickname = userInfo?.user_nickname;
+  let onHandlerSendMessage = () => {
+    const user_nickname = user_info?.user_nickname;
 
-  //   // const user_nickname = "ITZY 유나"
-  //   const avatar_url = userInfo?.avatar_url;
-  //   const id = userInfo?.id;
-  //   const email = userInfo?.email;
-  //   const created_at = dayjs().toISOString();
-  //   console.log("유저닉네임이 배열인가요 아닌가요??", user_nickname);
-  //   const messageInfo = {
-  //     user_nickname,
-  //     avatar_url,
-  //     message,
-  //     created_at,
-  //     id,
-  //     email,
-  //     chat_room_number,
-  //   };
-  //   socket.emit("sendMessage", {
-  //     roomnumber: chat_room_number,
-  //     messageInfo,
-  //   });
-  //   alert(
-  //     "메시지전송" +
-  //       JSON.stringify(messageInfo) +
-  //       "유저이름" +
-  //       JSON.stringify(messageInfo.user_nickname)
-  //   );
-  //   insertChatInfo({
-  //     user_nickname,
-  //     avatar_url,
-  //     message,
-  //     created_at,
-  //     chat_room_number,
-  //   });
-  //   setMessages(chat_room_number, [messageInfo]);
-  //   console.log("아니 시발 진짜 왜 안됨", user_nickname);
-  //   //방번호 보내기
-  // };
+    // const user_nickname = "ITZY 유나"
+    const avatar_url = user_info?.avatar_url;
+    const id = user_info?.user_id;
+    const email = user_info?.user_email;
+    const created_at = dayjs().toISOString();
+    console.log("유저닉네임이 배열인가요 아닌가요??", user_nickname);
+    const message_info = {
+      user_nickname,
+      avatar_url,
+      message,
+      created_at,
+      id,
+      email,
+      chat_room_number,
+    };
+    socket.emit("sendMessage", {
+      roomnumber: chat_room_number,
+      message_info,
+    });
+    alert(
+      "메시지전송" +
+        JSON.stringify(message_info) +
+        "유저이름" +
+        JSON.stringify(message_info.user_nickname)
+    );
+    // insertChatInfo({
+    //   user_nickname,
+    //   avatar_url,
+    //   message,
+    //   created_at,
+    //   chat_room_number,
+    // });
+    // setMessages(chat_room_number, [message_info]);
+    console.log("아니 시발 진짜 왜 안됨", user_nickname);
+    //방번호 보내기
+  };
 
   // const current_room_info = rooms[chat_room_number];
 
@@ -89,7 +95,7 @@ const ChatRoom = () => {
       <section>그 외의 유저들 </section>
       ChatRoom 입니다 안녕하세요
       <input placeholder="메세지를 입력해주세요"></input>
-      <button>전송</button>
+      <button onClick={onHandlerSendMessage}>전송</button>
     </div>
   );
 };
