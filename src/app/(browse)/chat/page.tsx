710c09cbf079e05f8ea764758035238ef01ce7d6
boyaneck <jinxx93@naver.com>
2025-03-07 03:42:11 +0900

    [CHORES] Emoji picker 창의 크기&위치 조정, 하트 생성 위치 수정

--- src/app/(browse)/chat/page.tsx
+++ src/app/(browse)/chat/page.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import ChatRoom from "./chat_room";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const ChatPage = () => {
+  const query_client = new QueryClient();
 
-  const query_client=new QueryClient()
-  
   return (
-    <div>
+    <>
       <QueryClientProvider client={query_client}>
-      
-      <ChatRoom />
-
+        <ChatRoom />
       </QueryClientProvider>
-    </div>
+    </>
   );
   //Ingress 와 Livekit 공부ㅎ
 };
