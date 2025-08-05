9b0f0f546a3dd4a2e4c461cce3760fae10aa5639
boyaneck <jinxx93@naver.com>
2025-08-05 23:11:48 +0900

    FE/streaming_page [CHORES] 스트리밍바의 아이콘 토글 값 관리를 위한 zustand완성

--- src/app/(browse)/live/_components/streamer_info_bar.tsx
+++ src/app/(browse)/live/_components/streamer_info_bar.tsx
@@ -13,24 +13,23 @@ interface StreamerInfoBarProps {
   items: NavItem[];
 }
 
-// 2. React.FC 없이, 함수의 인자에 직접 props 타입을 적용합니다.
 const StreamerInfoBar = ({ show, items }: StreamerInfoBarProps) => {
   return (
     // 부모를 기준으로 하단 중앙에 위치합니다.
     <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
       <div
         className={clsx(
-          ` h-8 rounded-xl mb-2 px-4
+          ` h-8 mb-2 px-4
             bg-white/10
             backdrop-blur-lg
-            border border-white/20
+            border border-white/20 rounded-xl
             shadow-lg
             flex items-center justify-center gap-4
-            transition-opacity duration-300`, // 투명도 변경 시 부드러운 전환 효과
+            transition-opacity duration-300`,
           {
-            "opacity-100": show, // show가 true일 때 나타남
-            "opacity-0": !show, // show가 false일 때 투명해짐
-            "animate-raiseUpBar": show, // 나타날 때 올라오는 애니메이션 적용
+            // "opacity-100": show,
+            "opacity-0": !show,
+            "animate-raiseUpBar": show,
           }
         )}
       >
