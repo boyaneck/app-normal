767955d33865c93d3cd0ebae6723eb6a24d2756a
boyaneck <jinxx93@naver.com>
2025-08-11 23:56:33 +0900

    Contribution 재정의  위한 commit

--- src/hooks/useStreamDuration.ts
+++ src/hooks/useStreamDuration.ts
@@ -64,6 +64,7 @@ export const useStreamDuration = ({
       }
     };
     const sss = "";
+    const ddd = "";
     //처음 접속시 스트리머 찾아서 시간 설정
     const streamer = room.getParticipantByIdentity(streamer_id);
     parseMetadata(streamer);
