a10ff3d05467b6b3245c8fe99b437d79c900c2fd
boyaneck <jinxx93@naver.com>
2025-08-28 23:42:55 +0900

    BE/streaming_page [CHORES] Redis 데이터 삽입 방식을 Hash 방식을 이용하여 입력,삭제 하도록 변경함

--- local_server/src/live/live_duration.js
+++ local_server/src/live/live_duration.js
@@ -13,45 +13,29 @@ export const livekitWebhook = async (req, res) => {
     //데이터 무결성을 check 한 뒤, buffer형태의 데이터를 json 객체로 변환
     const event = receiver.receive(req.body, req.get("Authorization"));
     const room_name = event.room.name;
-    console.log("RoomName", room_name);
-    console.log("Event 확인", event);
-    const redis_key = `stream${room_name}`;
-    console.log("Redis env 확인", process.env.REDIS_URL);
+    const redis_key = `${room_name}의 방송`;
     if (event.event === "track_published") {
-      // connectRedis();
-      //첫 스트리밍 시작일 경우
-      console.log("track published 일 경우만 ");
-      const room_exist = await redis_client_for_livekit.exists(redis_key);
-      const stream_data2 = {
-        start_time: Date.now(),
-        streamer_id: event.participant?.identity,
-        status: "LIVE",
-      };
-      await redis_client.set(redis_key, JSON.stringify(stream_data2));
-
-      if (room_exist === 0) {
-        const stream_data = {
-          //   start_time: Date.now(),
-          //   streamer_id: event.participant?.identity,
-          //   status: "LIVE",
-        };
-        // await redis_client.set(redis_key, JSON.stringify(stream_data));
-
-        console.log(
-          `🚀 Redis 저장  '${room_name}'의 방송 정보가 저장되었습니다.`
-        );
+      const room_exist = await redis_client_for_livekit.hSetNX(
+        redis_key,
+        event.participant?.identity,
+        `start_time:${Date.now()}`
+      );
+      console.log(
+        `🚀 Redis 저장  '${room_name}'의 방송 정보가 새롭게 저장되었습니다.`
+      );
+      if (room_exist === 1) {
+      } else if (room_exist === 0) {
+        console.log(`🔥이미 존재하는 데이터가 있습니다`);
       }
     }
-    //방송이 끝났다면
-    else if (event.event === "room_finished") {
+    //해당 데이터가 있을경우 그 다음 로직
+    if (event.event === "room_finished") {
       const del = await redis_client.del(redis_key);
-      if (del > 0) {
-        console.log(
-          `🗑️ Redis 삭제 '${room_name}'의 방송 정보가 삭제되었습니다.`
-        );
-      }
+      console.log(`🗑️ Redis 삭제 ${redis_key}의 방송 정보가 삭제되었습니다.`);
       console.log("방송 종료");
     }
+    res.status(200).send("Webhook processed successfully.");
+    console.log("마무리 코드 작동됨");
   } catch (error) {
     console.error("🔥 웹훅 처리 중 심각한 오류 발생:", error.message, error);
 
