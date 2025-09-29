012e55e9960226b66831d583fdc7852bde73c410
boyaneck <jinxx93@naver.com>
2025-09-29 23:52:18 +0900

    BE/studio_page [CHORES] 방송이 끝나면 해당 방송에 대한 정보를 redis에서 추출  및 supabse 삽입후 UI로 보여주는 작업중

--- local_server/src/live/live_participant.js
+++ local_server/src/live/live_participant.js
@@ -1,5 +1,7 @@
 import { WebhookReceiver } from "livekit-server-sdk";
 import { redis_client } from "../config/redis.js";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { insertAllParti, insertAvgParti, insertTopParti } from "../api/live.js";
 const api_key = process.env.LIVEKIT_API_KEY;
 const api_secret = process.env.LIVEKIT_API_SECRET;
 const receiver = new WebhookReceiver(api_key, api_secret);
@@ -13,16 +15,19 @@ export const liveParticipantWebhook = async (req, res) => {
     const event = receiver.receive(req.body, req.get("Authorization"));
     const room_name = event.room.name;
     const parti = event.participant.identity;
-    const redis_parti_key = `${room_name}:live`;
-    const redis_top_parti_key = `${room_name}:top_parti`;
-    const redis_avg_viewer_key = `${room_name}:avg_viewer`;
-    const redis_duration_key = `${room_name}:${parti}:duration`;
     if (event.event === "participant_joined") {
-      await redis_client.sAdd(redis_parti_key, parti);
-      const current_parti = await redis_client.sCard(redis_parti_key);
-      await redis_client.hSet(redis_top_parti_key, "top_parti", current_parti);
+      await redis_client.sAdd(`${room_name}:live`, parti);
+      const current_parti = await redis_client.sCard(`${room_name}:live`);
+      await redis_client.incr(`${room_name}:count`);
+
+      //최대 동시 시청자수
+      await redis_client.hSet(
+        `${room_name}:top_parti`,
+        "top_parti",
+        current_parti
+      );
       const current_top_parti = await redis_client.hGet(
-        redis_top_parti_key,
+        `${room_name}:top_parti`,
         "top_parti"
       );
 
@@ -30,37 +35,65 @@ export const liveParticipantWebhook = async (req, res) => {
         !current_top_parti ||
         current_parti > parseInt(current_top_parti, 10)
       ) {
-        await redis_client.hSet(redis_parti_key, "top_parti", current_parti);
+        await redis_client.hSet(
+          `${room_name}:top_parti`,
+          "top_parti",
+          current_parti
+        );
         console.log("동시접속자 수 업데이트", current_parti);
       }
+      //최대 동시 시청자수
 
       //평균 시청 지속률
       const start_at = Date.now();
-      await redis_client.hSet(redis_avg_viewer_key, parti, start_at);
+      await redis_client.hSet(`${room_name}:avg_viewer`, parti, start_at);
     } else if (event.event === "participant_left") {
+      await redis_client.decr(`${room_name}:count`);
       console.log("참여자 디버깅:", event);
 
       //평균 시청 지속률
       const end_at = Date.now();
-      const get_start_at = await redis_client.hGet(redis_avg_viewer_key, parti);
+      const get_start_at = await redis_client.hGet(
+        `${room_name}:avg_viewer`,
+        parti
+      );
 
       if (get_start_at) {
         const duration = Math.round((end_at - start_at) / 1000);
-        await redis_client.rPush(redis_duration_key, duration);
+        await redis_client.rPush(`${room_name}:${parti}:duration`, duration);
 
         //시청 지속률
-        conn_keep_rate = await redis_client.scan(redis_duration_key);
+        conn_keep_rate = await redis_client.scan(
+          `${room_name}:${parti}:duration`
+        );
       }
     } else if (event.event === "room_finished") {
       console.log(`${room_name} 방 종료 `);
 
       //지속 유저들/전체 입장한 유저들 = 평균 지속률률
-      const all_parti = await redis_client.sMembers(redis_parti_key);
+      const all_parti = await redis_client.sMembers(`${room_name}:live`);
       const rate = conn_keep_rate / all_parti;
     } else if (event.event === "ingress_ended") {
-      //redis에 저장된 해당 관련 방송 데이터들을 다 supabase 저장하기
-      await redis_client.rPush(`live_over${room_name}`, event);
       res.status(200).send("Webhook을 성공적으로 받았습니다.");
+
+      try {
+        const get_all_parti = await redis_client.sMembers(`${room_name}:live`);
+        const get_top_parti = await redis_client.hLen(`${room_name}:top_parti`);
+        const get_avg_parti = await redis_client.lRange(
+          `${room_name}:${parti}:duration`,
+          0,
+          -1
+        );
+      } catch (error) {}
+
+      try {
+        await insertAllParti(get_all_parti, room_name);
+        await insertTopParti(get_top_parti, room_name);
+        await insertAvgParti(get_avg_parti, room_name);
+      } catch (error) {}
+      // const get_chat_parti_ratio=await redis_client
+
+      return;
       //저장된 supabase의 데이터들을 가져와서 스트리머의 화면에 보여주기기
     }
     // ✅ 성공 응답을 보내야 합니다.
