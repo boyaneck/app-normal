54712b34c8606f7fd840bea5d1f808ef52ea618e
boyaneck <jinxx93@naver.com>
2025-10-16 23:51:56 +0900

    BE/studio_page [FEAT] 클라이언트에서 포트원 객체에 CustomData를 기입하고, 이를 웹훅을 통해 받아, redis key 설정에 필요한 세팅문제 해결

--- local_server/src/webhook.js
+++ local_server/src/webhook.js
@@ -1,62 +1,40 @@
 import axios from "axios";
-import { Buffer } from "buffer"; // Buffer 클래스 명시적 임포트
-
+import { Buffer } from "buffer"; // Buffer 클래스 명시적 임포
+import { redis_client } from "./config/redis.js";
 export const handleWebhook = async (req, res) => {
   try {
     const MAX_PAY = 1000000;
 
-    console.log("바디파싱", req.body);
-
     const tx_id = req.body.tx_id; //
-    const orderId = req.body.payment_id; //
-    const status = req.body.status;
-    console.log("ㅇㅇㅇㅇ", tx_id);
-    // 환경 변수 검증
-    const toss_secret_key = process.env.TOSS_SECRET;
-    const port_oneV2_store_id = process.env.PORT_ONE_STORE_KEY;
+    const payment_id = req.body.payment_id; //
     const port_oneV2_secret_key = process.env.PORT_ONE_SECRET_KEY;
 
-    if (!port_oneV2_secret_key || !port_oneV2_store_id) {
-      console.error("Webhook, 포트원 REST API 키가 누락되었습니다.");
-      // 웹훅 재전송을 막기 위해 200 OK 응답을 보냅니다.
-      return res.status(200).json({ message: "내부 서버 오류: 키 누락" });
-    }
-
-    //정보조회
-
-    // 환경
-
-    // 정보조회 요청 수정
     const getPayment = await axios({
-      // V2 API 주소 및 엔드포인트는 그대로 유지 (404가 아닌 401이 떴으므로 URL 자체는 존재한다고 가정)
-      url: `https://api.portone.io/payments?merchantUid=${orderId}`,
+      url: `https://api.portone.io/payments?paymentId=${payment_id}`,
       method: "get",
       headers: {
         "Content-Type": "application/json",
-        // 🚨 V2 공식 규격에 맞춰 Authorization 헤더 수정
         Authorization: `PortOne ${port_oneV2_secret_key}`,
-        // "Portone-Store-Id"와 "Portone-Secret" 헤더는 삭제
       },
     });
-    console.log("뀨우우우우우우우ㅜ우우우", getPayment);
     const payment_data = getPayment.data.payments
       ? getPayment.data.payments[0]
       : getPayment.data;
-    const actual_amount = payment_data.amount;
-    const payment_status = payment_data.status;
-    console.log("==========================================");
-    console.log(`[검증 결과] Merchant UID: ${orderId}`);
-    console.log(`[검증 결과] 결제 금액: ${actual_amount} 원`);
-    console.log(`[검증 결과] 결제 상태: ${payment_status}`);
-    console.log("==========================================");
-    console.log("payment데이터 확인", payment_data.items[0].amount.paid);
-
+    console.log(
+      "호스트 아이디",
+      JSON.parse(JSON.parse(getPayment.data.items[0].customData))
+    );
+    const get_parsed_custom_data = JSON.parse(
+      JSON.parse(getPayment.data.items[0].customData)
+    );
+    console.log("호스트 아이디 출력", get_parsed_custom_data.host_id);
     return res.status(200).json({
       status: "success",
       message: "결제 정보 조회 및 검증 완료",
     });
+
+    redis_client;
   } catch (e) {
-    // 🚨 API 오류 상세 로그 출력
     console.error(
       "웹훅 오류 발생. 상세:",
       e.response ? e.response.data : e.message
