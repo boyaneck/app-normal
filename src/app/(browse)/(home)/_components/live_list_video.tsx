1054fdcfc1ec1ea4e96509ae8285597ff26498e4
boyaneck <jinxx93@naver.com>
2026-01-20 23:44:40 +0900

    FE/home_page [CHORES] 메인베너 라이브영상 타이머 설정

--- src/app/(browse)/(home)/_components/live_list_video.tsx
+++ src/app/(browse)/(home)/_components/live_list_video.tsx
@@ -1,19 +1,17 @@
 import { useTrack, useTracks } from "@livekit/components-react";
-import { Track, VideoQuality } from "livekit-client";
+import { RemoteVideoTrack, Track, VideoQuality } from "livekit-client";
 import React, { useEffect } from "react";
 
 const LiveListVideo = () => {
-  const tracks = useTracks([Track.Source.Camera], { onlySubscribed: false });
-  const trackRef = tracks[0];
+  const tracks = useTracks([Track.Source.Camera], { onlySubscribed: true });
+  const track_reference = tracks[0];
 
   useEffect(() => {
-    if (trackRef && trackRef.publication) {
-      const pub = trackRef.publication;
-
-      pub.setVideoQuality(VideoQuality.LOW);
-      pub.setSubscribed(true);
+    const real_track = track_reference?.publication.track;
+    if (real_track instanceof RemoteVideoTrack) {
+      real_track.setVideoQuality(VideoQuality.LOW);
     }
-  }, []);
+  }, [track_reference]);
 
   return <div>LiveListVideo</div>;
 };
