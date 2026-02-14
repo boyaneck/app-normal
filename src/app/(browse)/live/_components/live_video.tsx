5b9512f1ee52fb0ec5fbeed80d4db2df0e728da2
boyaneck <jinxx93@naver.com>
2026-02-14 23:37:36 +0900

    FE/streaming_page [FIX] 비디오 태그의 스트림 detach,clean up 함수 추가로 메모리 누수 보완

--- src/app/(browse)/live/_components/live_video.tsx
+++ src/app/(browse)/live/_components/live_video.tsx
@@ -21,8 +21,13 @@ const LiveVideo = ({ participant }: LiveVideoProps) => {
 
   useEffect(() => {
     if (!videoRef.current) return;
-    if (is_playing) videoRef.current.play();
-    else videoRef.current.pause();
+    if (is_playing) {
+      videoRef.current.play().catch((err) => {
+        console.log("브라우저 정책으로 인해 video play 재생 실패", err);
+      });
+    } else {
+      videoRef.current.pause();
+    }
   }, [is_playing]);
 
   const controlVideoPlay = (e: React.SyntheticEvent<HTMLVideoElement>) => {
@@ -34,18 +39,40 @@ const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapper_ref = useRef<HTMLDivElement>(null);
 
+  // useEffect(() => {
+  //   console.log("비디오 컴포넌트의 트랙", videoRef.current);
+  // }, [participant]);
+  // useTracks([Track.Source.Microphone, Track.Source.Camera])
+  //   .filter((track) => track.participant.identity === participant.identity)
+  //   .forEach((track) => {
+  //     if (videoRef.current) track.publication.track?.attach(videoRef.current);
+  //   });
+  const tracks = useTracks([Track.Source.Camera]).filter(
+    (track) => track.participant.identity === participant.identity,
+  );
+
   useEffect(() => {
-    console.log("비디오 컴포넌트의 트랙", videoRef.current);
-  }, [participant]);
-  useTracks([Track.Source.Microphone, Track.Source.Camera])
-    .filter((track) => track.participant.identity === participant.identity)
-    .forEach((track) => {
-      if (videoRef.current) track.publication.track?.attach(videoRef.current);
-    });
+    const videoElement = videoRef.current;
+    const track = tracks[0]?.publication.track;
+
+    if (videoElement && track) {
+      track.attach(videoElement);
+      return () => {
+        track.detach(videoElement);
+      };
+    }
+  }, [tracks, participant.identity]); // 트랙이 바뀔 때만 실행되도록 관리
   return (
     <div>
       <div ref={wrapper_ref} className="relative h-full flex">
-        <video ref={videoRef} onPlay={controlVideoPlay} width="100%" />
+        <video
+          ref={videoRef}
+          onPlay={controlVideoPlay}
+          width="100%"
+          autoPlay
+          muted
+          playsInline
+        />
       </div>
     </div>
   );
