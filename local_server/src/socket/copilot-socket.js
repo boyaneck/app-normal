deaf21fe97c4fbd6540e997a053c4282a00e99d2
boyaneck <jinxx93@naver.com>
2026-06-16 23:11:57 +0900

    BE:[FEAT] AI 코파일럿- GROQ 연동 및 호스트 소켓 구조 수정

--- local_server/src/socket/copilot-socket.js
+++ local_server/src/socket/copilot-socket.js
@@ -1,1 +1,19 @@
-export const copilotSocket = () => {};
+export const copilotSocket = (socket, namespace_room) => {
+  //클라이언트에서오는 요청
+
+  let namespace = null;
+  namespace = namespace_room;
+
+  socket.on("copilot-connected", ({ hostId, roomName }) => {
+    socket.join(`copilot:${hostId}`);
+    console.log(`copilot:${hostId} 입장`);
+  });
+
+  //GROQ에서 응답 받은 걸로 오는
+};
+
+//GROQ로 부터 받은 해당 질문에 대한 INSIGHT 를 RETURN
+export const sendCopilotInsightToHost = ({ hostId, insightFromGROQ }) => {
+  if (!namespace) return;
+  namespace.to(`copilot:${hostId}`).emit(`copilotInsight`, insightFromGROQ);
+};
