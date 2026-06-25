faf69c74f35edaf5e3528da2991ca6c47c50d9b3
boyaneck <jinxx93@naver.com>
2026-06-25 20:18:34 +0900

    BE:[FEAT] 코파일럿 연결 토큰을 livekit 토큰으로 변경

--- src/api/token.ts
+++ src/api/token.ts
@@ -20,12 +20,12 @@ const createGuestUser = () => {
 };
 
 /**
- * @param host_id 현새 스트리머의 아이디
+ * @param hostID 현새 스트리머의 아이디
  * @returns 게스트 또는 해당 유저의 정보가 들어간 토큰을 발행
  */
-export const createViewerToken = async (host_id: string | undefined) => {
+export const createViewerToken = async (hostID: string | undefined) => {
   let now_user_info;
-  console.log("호스트의 아이디 확인하기", host_id);
+  console.log("호스트의 아이디 확인하기", hostID);
   try {
     //1.로그인시 supabase로 부터 세션 정보를 받아 브라우저 쿠키에 저장
     //2.next.js 서버에게 브라우저에서 받은 쿠키를 자동으로 넘겨줌
@@ -52,7 +52,7 @@ export const createViewerToken = async (host_id: string | undefined) => {
   }
 
   // 스트리머의 모든 정보 가져오기
-  const host = await getUserInfoById(host_id);
+  const host = await getUserInfoById(hostID);
   const is_host = now_user_info?.id === host?.id;
 
   // LiveKit 토큰 생성
@@ -60,19 +60,17 @@ export const createViewerToken = async (host_id: string | undefined) => {
     process.env.LIVEKIT_API_KEY,
     process.env.LIVEKIT_API_SECRET,
     {
-      identity: is_host ? `host-${now_user_info.id}` : now_user_info.id,
+      identity: is_host ? `HOST-${now_user_info.id}` : now_user_info.id,
       name: now_user_info.email,
-    }
+    },
   );
 
   token.addGrant({
-    room: host_id,
+    room: hostID,
     roomJoin: true,
     canPublish: is_host,
     canPublishData: true,
   });
-
-  console.log("만든 토큰 확인하기", token);
 
   const tokenStr = token.toJwt();
   console.log("타입은 무엇입니까 ??", typeof tokenStr);
