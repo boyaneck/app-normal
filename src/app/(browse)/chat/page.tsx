5fa50d9f75b0faf427d7db2641b80f9b3f89c042
boyaneck <jinxx93@naver.com>
2026-08-10 23:48:24 +0900

    [CHORES]

--- src/app/(browse)/chat/page.tsx
+++ src/app/(browse)/chat/page.tsx
@@ -6,9 +6,9 @@ interface Props {
   current_host_nickname: string;
   current_host_id: string;
 }
+const query_client = new QueryClient();
 
 const ChatPage = ({ current_host_nickname, current_host_id }: Props) => {
-  const query_client = new QueryClient();
   console.log("챗페이지에서의 콘솔", current_host_id);
 
   return (
